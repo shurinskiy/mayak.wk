915ed87e7a91da4323ac2264773ddd1c6c95f3ac
_tango <shurinskiy@mail.ru>
2023-10-17 20:00:18 +0600

    add behince icon

--- src/blocks/gallery/gallery.js
+++ src/blocks/gallery/gallery.js
@@ -1,6 +1,5 @@
 import { scrollClassToggle } from "../../js/libs/scroll";
 
-
 (() => {
 	const toggle = scrollClassToggle({ class: 'showed' });
 
--- src/blocks/hero/hero.js
+++ src/blocks/hero/hero.js
@@ -1,15 +1,17 @@
 import Swiper from 'swiper';
-import { Autoplay, EffectFade, Pagination } from 'swiper/modules';
+// import { Autoplay, EffectFade, Pagination } from 'swiper/modules';
+import { Autoplay, Pagination } from 'swiper/modules';
 
 (() => {
 
 	new Swiper(".hero__slider", {
-		modules: [ Autoplay, EffectFade, Pagination ],
+		// modules: [ Autoplay, EffectFade, Pagination ],
+		modules: [ Autoplay, Pagination ],
 		spaceBetween: 0,
 		loop: true,
 		speed: 2000,
-		effect: "fade",
-		fadeEffect: { crossFade: true },
+		// effect: "fade",
+		// fadeEffect: { crossFade: true },
 		autoplay: { 
 			delay: 10000,
 			disableOnInteraction: true
